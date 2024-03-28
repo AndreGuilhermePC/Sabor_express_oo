9216161d71a977d173421311aa72c4df113e50ff
Andre <Andre@gmail.com>
2024-03-28 14:55:33 -0300

    Criada a pasta Cardapio, as classes Prato e Bedida

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from modelos.restaurante import Restaurante
+from modelos.cardapio.prato import Prato
+from modelos.cardapio.bebida import Bebida
 
 restaurante_praca = Restaurante('praça', 'gourmet')
 #restaurante_mexicano = Restaurante('mexican food', 'mexicana')
@@ -6,12 +8,16 @@ restaurante_praca = Restaurante('praça', 'gourmet')
 
 #restaurante_mexicano.alterna_estado()
 
-restaurante_praca.receber_avaliacao('Gui', 10)
-restaurante_praca.receber_avaliacao('Lais', 8)
-restaurante_praca.receber_avaliacao('Emy', 2)
+#restaurante_praca.receber_avaliacao('Gui', 10)
+#restaurante_praca.receber_avaliacao('Lais', 8)
+#restaurante_praca.receber_avaliacao('Emy', 2)
+
+bebida_suco = Bebida('Suco de Mel', 5.0, 'Grande')
+prato_paozinho = Prato('Paozinho', 2.0, 'O melhor pão da cidade')
 
 def main():
-    Restaurante.lista_restaurante()
+    print(bebida_suco)
+    print(prato_paozinho)
 
 if __name__=='__main__':
     main()
